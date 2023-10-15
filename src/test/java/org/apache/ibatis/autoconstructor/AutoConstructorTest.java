97c43c7e2412a1b1f6f49e6f42e1f0e5a44d24ca
zpl <1639287627@qq.com>
2023-10-15 12:12:26 +0800

    1:增加注释

--- src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorTest.java
+++ src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorTest.java
@@ -15,13 +15,6 @@
  */
 package org.apache.ibatis.autoconstructor;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import java.io.Reader;
-import java.util.List;
-
 import org.apache.ibatis.BaseDataTest;
 import org.apache.ibatis.exceptions.PersistenceException;
 import org.apache.ibatis.executor.ExecutorException;
@@ -33,25 +26,32 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.Reader;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class AutoConstructorTest {
   private static SqlSessionFactory sqlSessionFactory;
 
   @BeforeAll
   static void setUp() throws Exception {
-    // create a SqlSessionFactory
+    // 基于配置文件创建sqlSessionFactory
     try (Reader reader = Resources.getResourceAsReader("org/apache/ibatis/autoconstructor/mybatis-config.xml")) {
       sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
     }
 
-    // populate in-memory database
-    BaseDataTest.runScript(sqlSessionFactory.getConfiguration().getEnvironment().getDataSource(),
-        "org/apache/ibatis/autoconstructor/CreateDB.sql");
+    // 初始化表接口和数据
+    BaseDataTest.runScript(sqlSessionFactory.getConfiguration()
+                                            .getEnvironment()
+                                            .getDataSource(), "org/apache/ibatis/autoconstructor/CreateDB.sql");
   }
 
   @Test
   void fullyPopulatedSubject() {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
+      // 因为该id对应的数据字段数据都不为空。所以不报错。如果查询数据。某个字段为空null也会导致报错。也是因为使用基本数据类型的原因
       final Object subject = mapper.getSubject(1);
       assertNotNull(subject);
     }
@@ -61,6 +61,7 @@ class AutoConstructorTest {
   void primitiveSubjects() {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
+      // 该查询会报错。因为数据库中有些值为null而类中数据类型都是用基本数据类型而不是包装类型。所有会导致报错
       assertThrows(PersistenceException.class, mapper::getSubjects);
     }
   }
@@ -68,6 +69,7 @@ class AutoConstructorTest {
   @Test
   void annotatedSubject() {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      // 基于注解  @AutomapConstructor构造器。因为该注解标注的构造器。使用了包装类型。不会报错
       final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
       verifySubjects(mapper.getAnnotatedSubjects());
     }
@@ -76,6 +78,7 @@ class AutoConstructorTest {
   @Test
   void badMultipleAnnotatedSubject() {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      // @AutomapConstructor只能应用于一个一个构造函数上。不能使用多个
       final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
       final PersistenceException ex = assertThrows(PersistenceException.class, mapper::getBadAnnotatedSubjects);
       final ExecutorException cause = (ExecutorException) ex.getCause();
@@ -86,6 +89,7 @@ class AutoConstructorTest {
   @Test
   void badSubject() {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      // 由于构造器对应的数据类型不一样。导致解析映射报错
       final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
       assertThrows(PersistenceException.class, mapper::getBadSubjects);
     }
@@ -101,6 +105,7 @@ class AutoConstructorTest {
 
   private void verifySubjects(final List<?> subjects) {
     assertNotNull(subjects);
-    Assertions.assertThat(subjects.size()).isEqualTo(3);
+    Assertions.assertThat(subjects.size())
+              .isEqualTo(3);
   }
 }
